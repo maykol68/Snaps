c8b3278af962ea28d638389102fc81234946fcc5
root <root@maymine>
2023-07-12 09:30:52 -0500

    added images

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -65,6 +65,6 @@ class ShotsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def shot_params
-      params.require(:shot).permit(:title, :color)
+      params.require(:shot).permit(:title, :color, :image)
     end
 end
